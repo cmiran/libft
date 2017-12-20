294e54ec2e3561d670bedbfa075c5156b5902e3f
cmiran <cmiran@student.42.fr>
2017-12-20 20:12:15 +0100

    rev ft_strcnew.c

--- ft_strcnew.c
+++ ft_strcnew.c
@@ -6,7 +6,7 @@
 /*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/20 14:33:55 by cmiran            #+#    #+#             */
-/*   Updated: 2017/12/20 14:36:13 by cmiran           ###   ########.fr       */
+/*   Updated: 2017/12/20 20:10:46 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@ char	*ft_strcnew(size_t size, char c)
 
 	if (!(str = (char *)malloc(sizeof(*str) * size + 1)))
 		return (NULL);
-	return (ft_memset(str, c, size + 1));
+	ft_memset(str, c, size);
+	str[size + 1] = '\0';
+	return (str);
 }
