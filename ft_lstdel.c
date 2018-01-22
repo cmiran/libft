b1c34b3b5b3a3f00c178918b773e4acf84ab8bb9
cmiran <cmiran@student.42.fr>
2018-01-22 18:02:01 +0100

    lol

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,11 +6,7 @@
 /*   By: cmiran <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/21 17:37:38 by cmiran            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2018/01/22 14:54:03 by cmiran           ###   ########.fr       */
-=======
-/*   Updated: 2018/01/22 17:47:53 by cmiran           ###   ########.fr       */
->>>>>>> 71a9c79bc3e0977a00de8fe5533b82554503d7c7
+/*   Updated: 2018/01/22 18:01:45 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +43,5 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	if ((*alst)->next)
 		ft_lstdel(&(*alst)->next, del);
-	printf("2\n");
 	ft_lstdelone(alst, del);
 }
