e1c2e589bb201901fcc75670e0379906108e5424
cmiran <cmiran@student.42.fr>
2019-06-26 00:29:28 +0200

    rev

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/12/03 20:42:11 by cmiran            #+#    #+#              #
-#    Updated: 2019/06/25 11:45:34 by cmiran           ###   ########.fr        #
+#    Updated: 2019/06/26 00:28:49 by cmiran           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,7 +18,7 @@ CFLAGS = -Wall -Werror -Wextra
 SRC = ft_atoi.c\
       ft_atoi_base.c\
       ft_bzero.c\
-	  ft_count_set_bit.c\
+	  ft_count_set_bits.c\
       ft_dupcstr.c\
       ft_freentab.c\
       ft_freetab.c\
--- ft_count_set_bits.c
+++ ft_count_set_bits.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_count_set_bit.c                                 :+:      :+:    :+:   */
+/*   ft_count_set_bits.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/24 11:15:03 by cmiran            #+#    #+#             */
-/*   Updated: 2019/06/24 15:06:09 by cmiran           ###   ########.fr       */
+/*   Updated: 2019/06/26 00:29:12 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_count_set_bit(size_t nbr)
+size_t	ft_count_set_bits(size_t nbr)
 {
 	size_t	count;
 
