4587c50cd6b0aa022129edbc0e8559a28a994a8e
cmiran <cmiran@student.42.fr>
2019-07-14 18:52:30 +0200

    rev

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cmiran <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/12 08:40:10 by cmiran            #+#    #+#             */
-/*   Updated: 2019/07/08 06:01:22 by cmiran           ###   ########.fr       */
+/*   Updated: 2019/07/14 17:28:21 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,32 @@
 # include <string.h>
 # include <unistd.h>
 
-# define BUFF_SIZE 1
+# define BUFF_SIZE			1
 
 # define BIT_SET(a, b)		((a) |= (1ULL<<(b)))
 # define BIT_CLEAR(a, b)	((a) &= ~(1ULL<<(b)))
 # define BIT_FLIP(a, b)		((a) ^= (1ULL<<(b)))
 # define BIT_CHECK(a, b)	(!!((a) & (1ULL<<(b))))
 # define IS_POWER_OF_TWO(n)	(n > 0 && !(n & (n - 1)))
+
+# define RESET				"\033[0m"
+# define BLACK				"\033[30m"			/* Black */
+# define RED				"\033[31m"			/* Red */
+# define GREEN				"\033[32m"			/* Green */
+# define YELLOW				"\033[33m"			/* Yellow */
+# define BLUE				"\033[34m"			/* Blue */
+# define MAGENTA			"\033[35m"			/* Magenta */
+# define CYAN				"\033[36m"			/* Cyan */
+# define WHITE				"\033[37m"			/* White */
+# define BOLDBLACK			"\033[1m\033[30m"	/* Bold Black */
+# define BOLDRED			"\033[1m\033[31m"	/* Bold Red */
+# define BOLDGREEN			"\033[1m\033[32m"	/* Bold Green */
+# define BOLDYELLOW			"\033[1m\033[33m"	/* Bold Yellow */
+# define BOLDBLUE			"\033[1m\033[34m"	/* Bold Blue */
+# define BOLDMAGENTA		"\033[1m\033[35m"	/* Bold Magenta */
+# define BOLDCYAN			"\033[1m\033[36m"	/* Bold Cyan */
+# define BOLDWHITE			"\033[1m\033[37m"	/* Bold White */
+# define UNDERLINE			"\033[4m"			/* Underline */
 
 typedef struct	s_list
 {
